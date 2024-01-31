89595f7f1d27f048a6e99e7b1915dd6b294bc393
oliver2214 <dima.tovstokor@yandex.ru>
2024-01-31 21:00:06 +0300

    Спарсил данные и занес в БД

--- Parsing/main.py
+++ Parsing/main.py
@@ -1,7 +1,9 @@
+from datetime import datetime
+import influxdb_client
 from parsers.parser1 import parse_data
 from influxdb_config import org, url, token
-import influxdb_client
 from influxdb_client.client.write_api import SYNCHRONOUS
+from influxdb_client import WritePrecision
 
 
 client = influxdb_client.InfluxDBClient(url=url,
@@ -15,11 +17,11 @@ write_api = client.write_api(write_options=SYNCHRONOUS)
 
 
 def main():
-    # Ваша основная логика здесь
-    parse_data()
+    record = parse_data("my_measurment")
 
-    p = influxdb_client.Point("my_measurement").tag("location", "Moscow").field("temperature", 11.0)
-    write_api.write(bucket=bucket, org=org, record=p)
+    # p = influxdb_client.Point("my_measurement").tag("location", "Moscow").field("temperature", 11.0)\
+    #    .time(seconds, WritePrecision.S)
+    write_api.write(bucket=bucket, org=org, record="\n".join(record))
 
 
 if __name__ == "__main__":
--- Parsing/parsers/parser1.py
+++ Parsing/parsers/parser1.py
@@ -1,3 +1,33 @@
+import requests
+import re
+from datetime import datetime
 
-def parse_data():
-    pass
+
+def parse_data(measurement):
+    year = 2024
+    month = 1
+    for day in range(8, 9):
+        date = f"{year}-{month}-{day}"
+
+        SECIDS = ["BANE", "BANEP", "VJGZ", "VJGZP", "GAZP", "RTGZ", "RTGZ", "EUTR", "LKOH", "MFGS", "MFGSP", "NVTK", "CHGZ", "ROSN", "RNFT", "KRKN", "KRKNP", "JNOSP", "JNOS", "SNGS", "SNGSP", "TATN", "TATNP", "TRNFP", "YAKG"]
+
+        text = ""
+        for start in range(0, 300, 100):  # отображение нужной информации осуществляется по 100 элементов на страницу, поэтому приходится циклом по 100 считывать по частям
+            url = f"https://iss.moex.com/iss/history/engines/stock/markets/shares/boards/tqbr/securities.xml?date={date}&start={start}"  # определяем url откуда будем парсить данные
+            text = text + requests.get(url).text  # получаем всю выборку
+
+        data = []
+
+        for i, line in enumerate(text.split("\n")):  # Пройдемся по всем строкам текста взятого с биржи для оптимизации, вместо поиска каждой необходимой акции
+            info = re.findall(r'SHORTNAME="(.+?)".+SECID="(.+?)".+VALUE="(.+?)".+OPEN="(.+?)".+LOW="(.+?)".+HIGH="(.+?)".+WAPRICE="(.+?)".+CLOSE="(.+?)"', line)  # тут происходит вычленение нужной информации со строки: цена, название и прочее
+            if info and info[0][1] in SECIDS:
+                timestamp = int(datetime(year, month, day).timestamp() * 1e9)
+                shortname = info[0][0].replace(" ", "\ ")
+                line = f'{measurement},SECID={info[0][1]},SHORTNAME="{shortname}" VALUE={info[0][2]},OPEN={info[0][3]},LOW={info[0][4]},HIGH={info[0][5]},WAPRICE={info[0][6]},CLOSE={info[0][7]} {timestamp}'
+                data.append(line)
+
+        return data
+
+
+if __name__ == "__main__":  # убрать
+    print(parse_data("a"))
